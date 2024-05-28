3785a0bbb358cee766ea7d339e42ef9c76b5b7a4
SerigneFallouNiang <fallouniang776@gmail.com>
2024-05-28 22:23:55 +0000

    ajout d'article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,4 +12,40 @@ class ArticleController extends Controller
         $articles = Article::all();
         return view ('articles.index' , compact('articles'));
     }
+    public function ajouter_article()
+    {
+        return view('articles.ajouter');
+    }
+
+    public function ajouter_article_traitement(Request $request)
+    {
+     
+        $request->validate([
+            
+            'nom'  => 'required',
+            'description'  => 'required',
+            'date_création'  => 'required',
+            'photo'  => 'required',
+            'valider'  => 'required',
+        ]);
+
+        $article = new Article();
+        $article->nom=$request->nom;
+        $article->description=$request->description;
+        $article->date_création=$request->date_création;
+        $article->photo=$request->photo;
+        $article->valider= $request->valider;
+
+
+        $article->save();
+
+        return redirect('/')->with('status','L\'article a bien été ajouté avec succes.');
+    }
+
+    public function delete_article($id)
+    {
+        $article=Article::find($id);
+        $article->delete();
+        return redirect('/')->with('status','L\'article a bien été supprimé avec succes.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\CommentaireController;
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ajouter', [ArticleController::class, 'ajouter_article']);
+Route::post('/ajouter/traitement', [ArticleController::class, 'ajouter_article_traitement']);
+
+Route::get('/delete-article/{id}', [ArticleController::class, 'delete_article']);
 
 
 Route::get('/', [ArticleController::class, 'index']);
